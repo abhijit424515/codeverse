9038d53891bf12dc3fa1b0169f7e6d0a8c4ba8ce
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2023-04-30 13:53:32 +0530

    added CSS lectures

---
+++ src/assets/Constants.tsx
@@ -0,0 +1,159 @@
+const lectures = [
+  {
+    title: "HTML",
+    data: [
+      {
+        title: "Introduction to HTML",
+        url: "https://drive.google.com/file/d/1pyae1YVo35qilQVNackCNPrVG89JOAow/preview",
+      },
+      {
+        title: "The Anatomy of an HTML Tag",
+        url: "https://drive.google.com/file/d/15WbKj22DEkwMzMx0B3eG-QRw89K_rmsg/preview",
+      },
+      {
+        title: "What we're building - HTML Personal Site",
+        url: "https://drive.google.com/file/d/1AK1UV5nS8cr5FfUs6_pdEA8taILkaO0R/preview",
+      },
+      {
+        title: "What is The HTML Boilerplate",
+        url: "https://drive.google.com/file/d/1VTBlsyxXIWzkjTGeoDQdo4MMjNlvbnld/preview",
+      },
+      {
+        title: "How to Structure Text in HTML",
+        url: "https://drive.google.com/file/d/1USkDP0SSf5_yTwh44UACYkdFdoWB6aVk/preview",
+      },
+      {
+        title: "HTML Lists",
+        url: "https://drive.google.com/file/d/17rbvcBFjdvQB8i9NxyEmHMsGNYA6lDV4/preview",
+      },
+      {
+        title: "HTML Image Elements",
+        url: "https://drive.google.com/file/d/1Csf-ls8xMsmxUhZwoNVXn5-n5shv08Fc/preview",
+      },
+      {
+        title: "HTML Links and Anchor Tags",
+        url: "https://drive.google.com/file/d/1_zaCy1uRK7mDd3S8ThoPcF21Lj4W_mwA/preview",
+      },
+      {
+        title: "HTML Tables",
+        url: "https://drive.google.com/file/d/13qgiX4V0OUjqo98SoLQ-jAtFdfcBeAKv/preview",
+      },
+      {
+        title: "Using HTML Tables for Layout",
+        url: "https://drive.google.com/file/d/17TPnQzwV_4ckCKZ9M6OEb7ZDPCO2re6F/preview",
+      },
+      {
+        title: "HTML Tables Code Challenge",
+        url: "https://drive.google.com/file/d/1c0yD5YHrncXJmr24Oqrr64lyF_rAoazI/preview",
+      },
+      {
+        title: "HTML Tables Solution Walkthrough",
+        url: "https://drive.google.com/file/d/1hMbFkUqYrGJfpjs1DeHRBi0kABXa4M-u/preview",
+      },
+      {
+        title: "HTML Forms",
+        url: "https://drive.google.com/file/d/1rwGt2b_KpjMtb52Ob05AKoQG_HJcKWfP/preview",
+      },
+      {
+        title: "Forms in Practice - Create a Contact Me Form",
+        url: "https://drive.google.com/file/d/1y4Ax4OePE0qFR6TqyNytcduca6Z6laDi/preview",
+      },
+    ],
+  },
+  {
+    title: "CSS",
+    data: [
+      {
+        title: "Introduction to CSS",
+        url: "https://drive.google.com/file/d/1InqFK2to7EblMbeF74CMlNa-1Yj_zAPu/preview",
+      },
+      {
+        title: "Inline CSS",
+        url: "https://drive.google.com/file/d/1LFb9ZxsOmNqs6BIlZ-2UnuA0Kr4VtZZU/preview",
+      },
+      {
+        title: "Internal CSS",
+        url: "https://drive.google.com/file/d/1RIxG87K9htYwXmDXok8VfuNON0qeZAFv/preview",
+      },
+      {
+        title: "External CSS",
+        url: "https://drive.google.com/file/d/1SelYoG8b9NdcgNsU2LPEglPDzjQfwt6Z/preview",
+      },
+      {
+        title: "How to Debug CSS Code",
+        url: "https://drive.google.com/file/d/1X7o0G3DJnGtnTlJC-umgp4q7VnHTsjn4/preview",
+      },
+      {
+        title: "The Anatomy of CSS Syntax",
+        url: "https://drive.google.com/file/d/1iIBGuQELTod7rQwY50o7JOWqNML9l8G7/preview",
+      },
+      {
+        title: "CSS Selectors",
+        url: "https://drive.google.com/file/d/1o9V7cPyfb15Hhg2u8tDTCRqCyZrGgUWq/preview",
+      },
+      {
+        title: "Classes vs. Ids",
+        url: "https://drive.google.com/file/d/1vAAsiZW6pZ8ToVylkL5XcjDhnxQLQDvY/preview",
+      },
+      //
+      {
+        title: "What We'll Make - Stylised Personal Site",
+        url: "https://drive.google.com/file/d/11CIZxloOiPAu9UC6rDAwf9FYSAKLBPMi/preview",
+      },
+      {
+        title: "What Are Favicons",
+        url: "https://drive.google.com/file/d/13I8nSBqkOrvNK1P0-o6bog-548Z4uaP9/preview",
+      },
+      {
+        title: "HTML Divs",
+        url: "https://drive.google.com/file/d/13wb2qIsqgKZT-hUfA5U2nGRiejthCpcU/preview",
+      },
+      {
+        title: "The Box Model of Website Styling",
+        url: "https://drive.google.com/file/d/1BeWo2DFLOSzZZ1PUC0_cc_MtzCOWIi5r/preview",
+      },
+      {
+        title: "CSS Display Property",
+        url: "https://drive.google.com/file/d/1HlVSnv7CROqxZZ8oN1kqKs9pqeeoHhvA/preview",
+      },
+      {
+        title: "CSS Static and Relative Positioning",
+        url: "https://drive.google.com/file/d/1NODEHqhK4YiEDGiEk2andI1ODULnqNay/preview",
+      },
+      {
+        title: "Absolute positioning",
+        url: "https://drive.google.com/file/d/1XrowZVweVGL8QlK9OcBtKBbiwxnJhCT1/preview",
+      },
+      {
+        title: "The Dark Art of Centering Elements with CSS",
+        url: "https://drive.google.com/file/d/1daBxd5UUu5XX-MBglsNlRWnCRMOSLea7/preview",
+      },
+      {
+        title: "Font Styling in Our Personal Site",
+        url: "https://drive.google.com/file/d/1fztYxGnrUVIB7DNLzkojpVJ5Q_pbo1KG/preview",
+      },
+      {
+        title: "Adding Content to Our Website",
+        url: "https://drive.google.com/file/d/1j5bohxSpwb671VnLNcAYfs0jJbykRWUe/preview",
+      },
+      {
+        title: "CSS Sizing",
+        url: "https://drive.google.com/file/d/1l0VloCBK5QvT9Aa0fk8JPFmBPkzFMnBv/preview",
+      },
+      {
+        title: "CSS Font Property Challenge Solutions",
+        url: "https://drive.google.com/file/d/1qBbFT3mS85FMG7T19O-Nay2W8K7U2WiH/preview",
+      },
+      {
+        title: "CSS Float and Clear",
+        url: "https://drive.google.com/file/d/1rR7Msncai17xO5IaPC4Gw9bVNu6rx9H6/preview",
+      },
+      {
+        title: "Stylised Personal Site Solution Walkthrough",
+        url: "https://drive.google.com/file/d/1rjIKJzpD4Mqom-C6zuUtvVelamZPxj69/preview",
+      },
+    ],
+  },
+];
+
+export default lectures;
